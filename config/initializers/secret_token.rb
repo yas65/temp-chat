39a35b75820217ce990cbae53a956812b1037b14
Kohei Yasumura <yasumura@f-plat.com>
2014-03-30 08:32:28 +0900

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TempChat::Application.config.secret_token = '76cdc7baf9553c224b8abc60f726731e7fb7c784a8328b15b39be25dc04d59aa481d8cba4d7b0a75deb34dc36dda3cc972e89798f877ec64f3898e4a0e0c0b34'
